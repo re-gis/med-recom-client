e386574f4dab9c2fa1051cfdf555d4fe5e32dd0c
re-gis <irumvaregisdmc@gmail.com>
2025-01-09 12:21:31 +0200

    [++]: encapsulation

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { BACKEND_API_URL } from '../constants';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/v1', // Replace with your backend URL
+  baseURL: BACKEND_API_URL, // Replace with your backend URL
 });
 
 export default api;
