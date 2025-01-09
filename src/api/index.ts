77beec6df93290169cc2186771cb4890c2595f04
re-gis <irumvaregisdmc@gmail.com>
2025-01-09 12:23:16 +0200

    [fix]: final

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { BACKEND_API_URL } from '../constants';
 
 const api = axios.create({
   baseURL: "https://med-recom-api.onrender.com/api/v1", // Replace with your backend URL
